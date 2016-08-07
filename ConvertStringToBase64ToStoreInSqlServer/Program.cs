283fb4b38cc7d7d7c8f3c5f33efed20bda536585
肖传令 <2890998007@qq.com>
2016-08-07 22:16:05 +0800

    读取txt文件中的敏感词并拼接sql,并使用Dapper导入到数据库中

---
+++ ConvertStringToBase64ToStoreInSqlServer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace ConvertStringToBase64ToStoreInSqlServer
+{
+    class Program
+    {
+        private static string conStr = "Data Source=db.frllk.com;Initial Catalog=Mealtime;Persist Security Info=True;User ID=sa;Password=***";
+        /// <summary>
+        /// 打开数据库链接方法
+        /// </summary>
+        /// <param name="constr"></param>
+        /// <returns></returns>
+        public static SqlConnection GetSqlConnection(string constr)
+        {
+            SqlConnection conn = new SqlConnection(constr);
+            conn.Open();
+            return conn;
+        }
+        static void Main(string[] args)
+        {
+            //1. 拼接sql
+            var builder=new StringBuilder();
+            using (var file=new StreamReader("smsBanned1.txt", Encoding.UTF8,true))
+            {
+                string line;
+                while ((line = file.ReadLine()) !=null)
+                {
+                    byte[] bytes = Encoding.Default.GetBytes(line);
+                    var afterStr = Convert.ToBase64String(bytes);
+                    builder.Append($"INSERT INTO IllegalWord ( Name, Type ) VALUES  ( '{afterStr}', '广告'  )\r\n");
+                }
+            }
+            //2. 记录生成的sql
+            File.AppendAllText("2.txt",builder.ToString(),Encoding.UTF8);
+            //3. sql执行
+            using (var conn=GetSqlConnection(conStr))
+            {
+               var effect= conn.Execute(builder.ToString());
+                Console.WriteLine($"导入：{effect}条敏感词");
+            }
+            Console.ReadKey();
+        }
+    }
+}
