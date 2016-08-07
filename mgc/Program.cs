829246c1887299756009c90aa778affc90c8394d
肖传令 <2890998007@qq.com>
2016-08-07 22:20:15 +0800

    敏感词算法

--- mgc/Program.cs
+++ mgc/Program.cs
@@ -28,7 +28,7 @@ namespace mgc
         /// <summary>
         /// 检查脏字
         /// </summary>
-        /// <param name="message">字符创</param>
+        /// <param name="message">字符串</param>
         /// <param name="placeholder">替换符</param>
         /// <returns></returns>
         static string CheckDirtyWord(string message,char placeholder)
@@ -38,32 +38,37 @@ namespace mgc
             //替换后的文本输出内容
             StringBuilder sb = new StringBuilder(message.Length);
 
-            //读取脏字源文件，存到字典中
-            using (StreamReader file = new StreamReader("bannerWoed1.txt", Encoding.UTF8, true))
-            {
-                string line;
-                while ((line = file.ReadLine()) != null)
-                {
-                    char value = line[0];
-                    if (dicList.ContainsKey(value))
-                        dicList[value].Add(line);
-                    else
-                        dicList.Add(value, new List<string>() { line });
-                }
-            }
-            //从到一个字符串数组中读取脏字；
+            #region 方式一：从txt文件（每行一个非法词）中读取，存到字典中
+            //using (StreamReader file = new StreamReader("bannerWoed1.txt", Encoding.UTF8, true))
+            //{
+            //    string line;
+            //    while ((line = file.ReadLine()) != null)
+            //    {
+            //        char value = line[0];
+            //        if (dicList.ContainsKey(value))
+            //            dicList[value].Add(line);
+            //        else
+            //            dicList.Add(value, new List<string>() { line });
+            //    }
+            //} 
+            #endregion
+
+            #region 方式二：从txt文件中（字符串用"|"分割）读取脏字；
             //string filterText = "需要过滤的脏字 以|分开";
             ////脏字 可根据自己的方式用分隔符
-            //string[] filterData = filterText.Split('|');
+            //string[] filterData = filterText.Split(new char[] { '|' },StringSplitOptions.RemoveEmptyEntries);
             //foreach (var item in filterData)
             //{
             //    char value = item[0];
             //    if (dicList.ContainsKey(value))
             //        dicList[value].Add(item);
             //    else
-            //        dicList.Add(value, new List<string>() {item});
-            //}
+            //        dicList.Add(value, new List<string>() { item });
+            //} 
+            #endregion
+            #region 方式三：从数据库中读取
 
+            #endregion
             //检查输入的文本是否包含脏字
             int count = message.Length;
             for (int i = 0; i < count; i++)
